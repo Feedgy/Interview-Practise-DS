3c06f1a2e905da9ab7ed2fce95dfd07455e2761a
Nicolas Favrot <n.favrot@gmail.com>
2024-05-28 12:10:09 +0200

    feat: setup repo

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from pathlib import Path
+
+data_path = Path("data/")
+interview_dataset = Path("interview_dataset.pickle")
+pickle_filepath = data_path / interview_dataset
+
+# 1. Visualise plant data, perform EDA, explain your findings
+
+# 2. From what you found how would you clean data?
+
+## plot
+
+## filter
+
+# 3. (Bonus) Do regression model to predict the next hour production
